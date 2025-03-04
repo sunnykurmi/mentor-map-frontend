4921bb6b77a6a6d9dfe8eee425ade6131f4baf49
sunnykurmi <sunnykurmiskks@gmail.com>
2025-03-05 00:28:15 +0530

    updated axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:8080/",
+  // baseURL: "http://localhost:8080/",
+  baseURL: "https://mentor-map-backend.vercel.app/",
 });
 
 export default instance;
